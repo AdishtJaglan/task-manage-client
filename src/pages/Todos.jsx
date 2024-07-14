cc6475862d9346f0888a8eed4f4e9b8d2088df5f
AdishtJaglan <adisht7524@gmail.com>
2024-07-14 17:44:09 +0530

    create tasks page

---
+++ src/pages/Todos.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getTodos, deleteTodo } from "../services/api";
+
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const response = await getTodos();
+      setTodos(response.data);
+    }
+
+    fetchTodos();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteTodo(id);
+    setTodos(todos.filter((todo) => todo.id != id));
+  };
+
+  return (
+    <div>
+      <h2>Todos</h2>
+      <Link to="/todos/create">Create Todo</Link>
+
+      <ul>
+        {todos.map((todo) => {
+          <li key={todo.id}>
+            {todo.title}
+            <Link to={`/todos/update/${todo.id}`}>Update</Link>
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
+}
